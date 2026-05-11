4386278990b8ece52ff55adf6c3b03783135e835
elricgonza <elricgonza@gmail.com>
2026-05-11 17:07:16 -0400

    cont deploy bd load

--- run.py
+++ run.py
@@ -6,10 +6,12 @@ app = create_app()
 def inicializar_si_necesario():
     with app.app_context():
         from app.models import Usuario
+        print('test...')
         try:
             # Si no hay usuarios, inicializar
-            if not Usr.query.first():
+            if not Usuario.query.first():
                 import subprocess
+                print('Inicializando la base de datos...')
                 subprocess.run(['python', 'scripts/init_db.py'], check=True)
         except Exception as e:
             print(f"BD aún no disponible o ya inicializada: {e}")
