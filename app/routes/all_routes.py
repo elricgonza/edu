98de5ba19de0daeb5edd2d4e4d6d18812fbd51ee
elricgonza <elricgonza@gmail.com>
2026-06-11 16:48:18 -0400

    unique upd my - 4 líneas

--- app/routes/all_routes.py
+++ app/routes/all_routes.py
@@ -474,15 +474,16 @@ def nuevo():
         )
         db.session.add(ins); db.session.flush()
         costo = Costo.query.filter_by(cur_id=ins.cur_id).first()
-        if costo:
-            for i in range(1, costo.nro_cuota + 1):
-                monto = float(costo.cuota) * (1 - ins.descuento / 100)
-                p = Pago(ins_id=ins.id, nro_cuota=i, cuota=round(monto, 2),
-                         pagado=False, creado=datetime.now(), act=datetime.now(), usu_id=current_user.id)
-                db.session.add(p)
+        if ins.descuento != 100:
+            if costo:
+                for i in range(1, costo.nro_cuota + 1):
+                    monto = float(costo.cuota) * (1 - ins.descuento / 100)
+                    p = Pago(ins_id=ins.id, nro_cuota=i, cuota=round(monto, 2),
+                             pagado=False, creado=datetime.now(), act=datetime.now(), usu_id=current_user.id)
+                    db.session.add(p)
         db.session.commit()
         log_accion('CREATE', 'inscrito', entidad_id=ins.id, detalle={'alumno': ins.alumno.nombre_completo, 'cur_id': ins.cur_id, 'inscrito': ins.inscrito, 'reserva': ins.reserva})
-        flash('Inscripción realizada. Plan de pagos generado.', 'success')
+        flash('Inscripción realizada. (Plan de pagos generado si descuento diferente a 100%.', 'success')
         return redirect(url_for('inscrito.index'))
     return render_template('inscrito/form.html', inscrito=None, alumnos=alumnos, cursos=cursos)
 
@@ -973,7 +974,10 @@ def actualiza_plan():
 
     if request.method == 'POST':
         candidatos = Inscrito.query.filter(
-            db.or_(Inscrito.reserva == True, Inscrito.inscrito == True)
+            db.and_(
+                db.or_(Inscrito.reserva == True, Inscrito.inscrito == True),
+                Inscrito.descuento != 100
+            )
         ).all()
 
         actualizados  = 0   # alumnos a los que se les añadieron cuotas faltantes
